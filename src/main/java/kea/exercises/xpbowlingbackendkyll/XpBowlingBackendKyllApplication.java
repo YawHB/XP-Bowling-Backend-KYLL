04c6e6c81fd136c26452d3324579d31c448a174e
YawHB <yawhboateng@gmail.com>
2024-05-10 15:11:08 +0200

    Initial commit

---
+++ src/main/java/kea/exercises/xpbowlingbackendkyll/XpBowlingBackendKyllApplication.java
@@ -0,0 +1,13 @@
+package kea.exercises.xpbowlingbackendkyll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XpBowlingBackendKyllApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XpBowlingBackendKyllApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kea/exercises/xpbowlingbackendkyll/XpBowlingBackendKyllApplicationTests.java
@@ -0,0 +1,13 @@
+package kea.exercises.xpbowlingbackendkyll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XpBowlingBackendKyllApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
