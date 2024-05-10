af78136aa0a88060d3deee1d11443f4f874a8f7a
YawHB <yawhboateng@gmail.com>
2024-05-10 19:00:06 +0200

    deployment-test hello

--- src/main/java/kea/exercises/xpbowlingbackendkyll/controller/Controller.java
+++ src/main/java/kea/exercises/xpbowlingbackendkyll/controller/Controller.java
@@ -1,4 +1,14 @@
 package kea.exercises.xpbowlingbackendkyll.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//@RestController
 public class Controller {
+    @GetMapping("/hello")
+    public String index() {
+    return "Hello World!";
+}
+
+
 }
