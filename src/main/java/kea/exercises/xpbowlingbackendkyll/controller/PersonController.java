b447aca241c9472873085d79a34c2fb78688b7d8
YawHB <yawhboateng@gmail.com>
2024-05-10 20:43:55 +0200

    Code clean up and deleting files

--- src/main/java/kea/exercises/xpbowlingbackendkyll/controller/PersonController.java
+++ src/main/java/kea/exercises/xpbowlingbackendkyll/controller/PersonController.java
@@ -1,14 +1,16 @@
 package kea.exercises.xpbowlingbackendkyll.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Controller {
-    @GetMapping("/hello")
-    public String index() {
-    return "Hello World!";
-}
+public class PersonController {
 
 
+
+    @GetMapping("/person")
+    public String getPerson() {
+        return "Hello Person";
+    }
 }
