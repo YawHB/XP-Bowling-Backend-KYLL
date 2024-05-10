bfaf10a6feb94455c09f2ff4130bd7f6f86ddc35
YawHB <yawhboateng@gmail.com>
2024-05-10 19:22:12 +0200

    testing with Restcontroller

--- src/main/java/kea/exercises/xpbowlingbackendkyll/controller/Controller.java
+++ src/main/java/kea/exercises/xpbowlingbackendkyll/controller/Controller.java
@@ -3,7 +3,7 @@ package kea.exercises.xpbowlingbackendkyll.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@RestController
+@RestController
 public class Controller {
     @GetMapping("/hello")
     public String index() {
