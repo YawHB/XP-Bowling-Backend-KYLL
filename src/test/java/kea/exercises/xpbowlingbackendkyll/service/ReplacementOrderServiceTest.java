4af6f1bbc83257712ceeda1a88ba66b1ab9f6c17
LisePetculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-05-26 14:55:59 +0200

    lidt kød på integrations test i stock

--- src/test/java/kea/exercises/xpbowlingbackendkyll/service/ReplacementOrderServiceTest.java
+++ src/test/java/kea/exercises/xpbowlingbackendkyll/service/ReplacementOrderServiceTest.java
@@ -17,10 +17,12 @@ class ReplacementOrderServiceTest {
     private WebTestClient webTestClient;
 
 
-    @Test
-    void createReplacementOrder() {
-        webTestClient.post().uri("/replacementorders").contentType(MediaType.APPLICATION_JSON)
-                .bodyValue("""
+
+@Test
+void createReplacementOrderCheckID() {
+    webTestClient.post().uri("/replacementorders").contentType(MediaType.APPLICATION_JSON)
+            .bodyValue("""
+                        
 {
     "replacementOrder": {
             "title": "test title",
@@ -37,10 +39,47 @@ class ReplacementOrderServiceTest {
 
 }
 
-        """)
-                .exchange()
-                .expectStatus().isCreated()
-                .expectBody(String.class)
-                .consumeWith(response -> System.out.println(response.getResponseBody()));
-    }
+    """)
+            .exchange()
+            .expectStatus().isCreated()
+            .expectBody(String.class)
+            .consumeWith(response -> {
+                String responseBody = response.getResponseBody();
+                assertNotNull(responseBody);
+                assertTrue(responseBody.contains("id"));
+                System.out.println(responseBody);
+            });
+}
+
+
+
+//    @Test
+//    void createReplacementOrder() {
+//        webTestClient.post().uri("/replacementorders").contentType(MediaType.APPLICATION_JSON)
+//                .bodyValue("""
+//
+//                        {
+//    "replacementOrder": {
+//            "title": "test title",
+//            "totalPrice": 200.0,
+//            "timeDate": "2024-05-19T19:56:51.015"
+//         },
+//    "orderItems": [
+//        {
+//            "amountToOrder": 6,
+//            "stockItem": {"name": "Bowling-shoe-pair size 38"}
+//        }
+//
+//    ]
+//
+//}
+//
+//        """)
+//                .exchange()
+//                .expectStatus().isCreated()
+//                .expectBody(String.class)
+//                .consumeWith(response -> System.out.println(response.getResponseBody()));
+//    }
+
+
 }
