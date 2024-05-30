e7457d4683e81b5804fbc0038ea701d629625775
YawHB <yawhboateng@gmail.com>
2024-05-30 21:10:40 +0200

    Created test that ignores consumable that is created with an id and refactored test names

--- src/test/java/kea/exercises/xpbowlingbackendkyll/repository/consumable/ConsumableRepositoryTest.java
+++ src/test/java/kea/exercises/xpbowlingbackendkyll/repository/consumable/ConsumableRepositoryTest.java
@@ -14,7 +14,6 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
-// uses a virtue database instead of the actual database for testing purposes
 @AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class ConsumableRepositoryTest {
     @Autowired
@@ -22,29 +21,70 @@ class ConsumableRepositoryTest {
 
 
     @Test
-    void ConsumableRepository_SaveConsumable_ReturnSavedConumable() {
+    void shouldCreateConsumableWithExpectedAttributes() {
 
-        //Arrange
-        // builder pattern builds the object with the given parameters
         Consumable wine = Consumable.builder().
                 title("Wine").
                 price(50).
                 build();
 
-
         //Act
-
         Consumable savedWine = consumableRepository.save(wine);
-
 
         //Assert
         assertNotNull(savedWine);
         assertTrue(savedWine.getId() > 0);
+
+
+    }
+
+    @Test
+    void shouldIgnoreIdWhenCreatingNewConsumable() {
+        //Arrange
+        Consumable wine = Consumable.builder().
+                title("yaw").
+                price(50).
+                id(10).
+                build();
+
+        //Act
+        Consumable savedWine = consumableRepository.save(wine);
+
+        //Assert
+        assertNotNull(savedWine);
+        assertNotEquals(10, savedWine.getId());
+        System.out.println("The id is " + savedWine.getId());
     }
 
 
     @Test
-    void ConsumableRepository_SaveAllConsumables_ReturnConsumable() {
+    void shouldUpdateConsumableAttributesCorrectly() {
+        //Arrange
+        Consumable peanuts = Consumable.builder().
+                title("Peanuts").
+                price(10).
+                build();
+
+        Consumable savedWine = consumableRepository.save(peanuts);
+        System.out.println("The price was " + peanuts.getPrice());
+
+        //Act
+
+        savedWine.setPrice(20);
+
+        Consumable updatedPeanuts = consumableRepository.save(savedWine);
+
+        //Assert
+        assertEquals(20, updatedPeanuts.getPrice());
+        System.out.println("The price is " + updatedPeanuts.getPrice());
+
+
+    }
+
+
+
+    @Test
+    void shouldSaveAllConsumablesAndReturnSameConsumables() {
         List<Consumable> products = new ArrayList<>();
         //Arrange
         products.add(Consumable.builder().
@@ -62,17 +102,10 @@ class ConsumableRepositoryTest {
                 price(15).
                 build());
 
-
-
         //Act
-
         List<Consumable> savedProducts = consumableRepository.saveAll(products);
 
         //Assert
         assertEquals(products, savedProducts);
-
-
-
-
     }
 }
